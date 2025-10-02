7ade12e36a5f12723a917bf356753c05bca87c38
Sofia HAZAMI <sofia.hazami@insystem.fr>
2025-10-02 16:54:22 +0200

    Navbar Tab

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -54,20 +54,16 @@ export default function Navbar() {
             />
 
             <svg
-              className={`swap-off ${scrolled ? "fill-black" : "fill-bg"}`}
+              className={`swap-off ${scrolled ? "fill-black" : "fill-bg"} w-[24px] h-[24px] md:w-[38px] md:h-[38px]`}
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
               viewBox="0 0 512 512"
             >
               <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
             </svg>
 
             <svg
-              className={`swap-on fill-bg`}
+              className={`swap-on fill-bg  w-[24px] h-[24px] md:w-[38px] md:h-[38px]`}
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
               viewBox="0 0 512 512"
             >
               <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
@@ -75,7 +71,8 @@ export default function Navbar() {
           </label>
           <ul
             tabIndex="0"
-            className={`menu bg-menu absolute top-0 text-white-text divide-y-1 divide-white-text font-artemisia text-sm w-50 h-60 py-12 items-center  my-slide-menu ${
+            className={`menu bg-menu absolute top-0 text-white-text divide-y-1 divide-white-text font-artemisia text-sm w-50 h-60 py-12 items-center my-slide-menu md:w-[40vw] md:h-[30vh] md:text-lg
+              ${
               menuOpen ? "open" : "closed"
             }`}
             style={{ zIndex: 60 }}
@@ -119,12 +116,12 @@ export default function Navbar() {
           <img
             src={scrolled ? LogoTrigantNoir : LogoTrigantBlanc}
             alt="Trigant Logo"
-            className={`w-40`}
+            className={`w-40 md:w-70`}
           />
           <img
             src={scrolled ? pessacLogoNoir : pessacLogoBlanc}
             alt="Pessac-Leognan Logo"
-            className={`w-30`}
+            className={`w-30 md:w-50`}
           />
         </Link>
       </div>
