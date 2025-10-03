6325400e98a6934e3820f98315220aea1becfaf1
Sofia HAZAMI <sofia.hazami@insystem.fr>
2025-10-03 18:07:19 +0200

    Mariage PC

--- src/components/galerie.jsx
+++ src/components/galerie.jsx
@@ -1,6 +1,6 @@
 export default function Galerie({ images }) {
   return (
-    <div className="grid gap-2 grid-cols-2 mx-2 mt-[var(--space-small)]">
+    <div className="grid gap-2 grid-cols-2 mx-2 mt-[var(--space-small)] lg:grid-cols-4">
       {images.map((img, index) => (
         <div key={index} className="overflow-hidden rounded-lg">
           <img src={img} alt={`Photo ${index + 1}`} className="w-full h-full object-cover"/>
--- src/pages/mariage.jsx
+++ src/pages/mariage.jsx
@@ -13,9 +13,11 @@ import maries from "../assets/images/galerieMariage/maries.jpg";
 import cour_nuit from "../assets/images/galerieMariage/cour_nuit.png";
 import tables from "../assets/images/galerieMariage/tables.jpg";
 import room from "../assets/images/galerieMariage/room.jpg";
+import dance from "../assets/images/galerieMariage/dance.jpg"
+import bathroom from "../assets/images/galerieMariage/bathroom.jpg"
 
 export default function Mariage() {
-  const images = [pelouse, toast, maries, cour_nuit, tables, room];
+  const images = [pelouse, toast, maries, cour_nuit, tables, dance, room, bathroom];
 
   return (
     <div>
@@ -41,7 +43,7 @@ export default function Mariage() {
         <img
           src={separateur}
           alt="separateur"
-          className="my-[var(--space-big)] w-80 md:w-[58vw]"
+          className="my-[var(--space-big)] w-80 md:w-[58vw] lg:w-[25vw] lg:my-[var(--space-big-lg)]"
         />
       </div>
 
@@ -49,8 +51,8 @@ export default function Mariage() {
         <h1 className="title px-4">Le Domaine en Quelques Chiffres</h1>
 
         <div className="md:flex">
-          <div className="border-2 border-accent-gold rounded-[20px] w-[90vw] mt-[var(--space-small)] mx-auto flex flex-col items-center md:w-[44vw]">
-            <h2 className="mini-title" style={{ paddingTop: "2vh" }}>
+          <div className="border-2 border-accent-gold rounded-[20px] w-[90vw] mt-[var(--space-small)] mx-auto flex flex-col items-center md:w-[44vw] lg:w-[35vw]">
+            <h2 className="mini-title other-title" style={{ paddingTop: "2vh" }}>
               Dans le Château
             </h2>
             <ul
@@ -69,12 +71,12 @@ export default function Mariage() {
             </ul>
           </div>
 
-          <div className="border-2 border-accent-gold rounded-[20px] w-[90vw] mt-[var(--space-small)] mx-auto flex flex-col items-center md:w-[44vw]">
-            <h2 className="mini-title" style={{ paddingTop: "2vh" }}>
+          <div className="border-2 border-accent-gold rounded-[20px] w-[90vw] mt-[var(--space-small)] mx-auto flex flex-col items-center md:w-[44vw] lg:w-[35vw]">
+            <h2 className="mini-title other-title" style={{ paddingTop: "2vh" }}>
               À l'extérieur
             </h2>
             <ul
-              className="list-disc list-inside mini-text"
+              className="list-disc list-inside mini-text" 
               style={{
                 paddingTop: "2vh",
                 width: "80%",
@@ -92,7 +94,7 @@ export default function Mariage() {
         </div>
       </div>
 
-      <div className="my-[var(--space-small)]">
+      <div className="my-[var(--space-small)] lg:my-[var(--space-big-lg)]">
         <h1 className="title">Galerie Photos</h1>
         <Galerie images={images} />
       </div>
